21527e9304a5a5fec125d23e49c3382c784b7e3c
jincsama <jinc2020@outlook.com>
2024-03-09 23:52:42 +0800

    地址簿管理

--- sky-server/src/main/java/com/sora/controller/admin/CommonController.java
+++ sky-server/src/main/java/com/sora/controller/admin/CommonController.java
@@ -3,6 +3,7 @@ package com.sora.controller.admin;
 import com.sora.constant.MessageConstant;
 import com.sora.result.Result;
 import com.sora.utils.COSUtil;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +20,7 @@ import java.util.UUID;
 @Slf4j
 @RestController
 @RequestMapping("/admin/common")
-@ApiOperation("通用接口")
+@Api(tags = "通用接口")
 public class CommonController {
 
     @Autowired
@@ -32,13 +33,27 @@ public class CommonController {
      */
     @PostMapping("/upload")
     @ApiOperation("文件上传")
-    public Result<String> upload(MultipartFile file){
+    public Result<String> upload(MultipartFile file)throws Exception {
         log.info("上传文件:{}", file);
         String originalFilename = file.getOriginalFilename();
         File fileJ = new File(originalFilename);
         String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
         String objectName = UUID.randomUUID().toString() + extension;
 
+        //上传腾讯云
+        return COSupload(file, fileJ, objectName);
+
+        //上传本地
+//        String uploadDir = "D:\\c\\work\\sky-take-out\\uploaded\\images\\"; // 指定上传文件保存的目录
+//        String filePath = uploadDir + objectName; // 拼接文件路径
+//
+//        File destination = new File(filePath);
+//        file.transferTo(destination);
+//
+//        return Result.success(filePath);
+    }
+
+    private Result<String> COSupload(MultipartFile file, File fileJ, String objectName) {
         // 方法1: 使用 FileCopyUtils.copy 方法
         try {
             FileCopyUtils.copy(file.getBytes(), fileJ);
